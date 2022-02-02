a49d40f44d35890da62489fbd721e8f52096977b
Nivaz <mail.nivaz@gmail.com>
2022-02-02 23:16:32 +0530

    like app

--- store/models.py
+++ store/models.py
@@ -1,9 +1,20 @@
-from turtle import title
 from django.db import models
 
 # Create your models here
 
+#Promotions class
 
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+#Collection class
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+    
 #Product class
 
 class Product(models.Model):
@@ -12,6 +23,11 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+
+   
 
 #Customer class
 
@@ -34,7 +50,19 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
- #Order class
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+
+
+#Address - customer address class
+
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+
+
+
+#Order class
 
 class Order(models.Model):
 
@@ -50,3 +78,20 @@ class Order(models.Model):
 
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_PENDING)
+
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+
+#Cart class
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+#Item class
+
+class Item(models.Model):
+   cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+   product = models.ForeignKey(Product, on_delete=models.CASCADE)
+   quantity = models.PositiveSmallIntegerField()
--- tags/models.py
+++ tags/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+#Tag
+
+class Tag(models.Model):
+    label = models.CharField(max_length=255)
+
+
+# Tagged Items
+
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
